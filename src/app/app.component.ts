bf4af18e1c10611fcc9747efa66988a0394a2f6f
Facundo Merighi <facundo.merighi@globant.com>
2020-05-15 12:30:53 -0300

    Adding component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, Input } from '@angular/core';
+import { ajax } from 'rxjs/ajax';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'run-statistics';
+  activities: any[];
+  activitiesFiltered: any[];
+  filterField: string = '';
+  filterValue: number = 0;
+  doFilter() {
+    this.activitiesFiltered = this.activities;
+    if (this.filterField) {
+      this.activitiesFiltered =  this.activities.filter((activity) => activity[this.filterField] == this.filterValue);
+    }
+    
+  }
+  getMins (activity) {
+    const mins = activity.duration / 60000;
+    return Math.trunc(mins);
+  }
+  getPace(activity) {
+    return activity.distance / activity.duration;
+  }
+  getSec (activity) {
+    const mins = activity.duration / 60000;
+    const secs = mins % 1;
+
+    return Math.trunc(secs * 60);
+  }
+  handleData(data) {
+     this.activities = data.filter((activity) => activity.distance > 0);
+    this.activitiesFiltered = this.activities.slice();
+  }
+
+  ngOnInit() {
+    ajax('http://localhost:3000/api/activities')
+  
+  .subscribe(ajaxResponse => this.handleData(ajaxResponse.response));
+  }
+
+
+  
+
 }
+
+
